c53d3b78d2c7421ea685b8d6be7d69634d446812
victorAkila <87238260+victorAkila@users.noreply.github.com>
2021-11-05 19:02:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
     
 @app.route('/EJ')
 def EJ():
